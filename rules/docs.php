3582c331cb7312db60bc44d73a7ff03a70c12946
fago <fago>
2008-07-24 13:02:31 +0000

    * renamed the table for storing rule sets. reinstall the module  * added a file for some further doxygen comments, still empty  * first catch of the input evaluator system:     - added a php input evaluator, which is activated when the php module is activated     - ported the drupal set message action as it's useful for testing the     - help for the evaluators is missing, coming soon.

---
+++ rules/docs.php
@@ -0,0 +1,33 @@
+<?php
+// $Id$
+/**
+ * @file
+ * This file contains no working PHP code; it exists to provide additional documentation
+ * for doxygen as well as to document hooks in the standard Drupal manner.
+ */
+
+/**
+ * @defgroup rules_hooks Rules' hooks
+ * @{
+ * Hooks that can be implemented by other modules in order to extend the rules module.
+ */
+
+/**
+ * This hook should be placed in MODULENAME.rules.inc and it will be auto-loaded.
+ */
+function hook_rules_action_info() {
+  // example code here
+}
+
+/**
+ * This hook should be placed in MODULENAME.rules.inc and it will be auto-loaded.
+ */
+function hook_rules_action_info() {
+  // example code here
+}
+
+
+
+/**
+ * @}
+ */
