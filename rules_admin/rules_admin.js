02677087b4c30c7141a54699658e9d27030e5906
Wolfgang Ziegler <nuppla@zites.net>
2010-11-25 11:14:53 +0000

    #874802 patch by fabsor: Allow user to specify rules' machine-readable names, as with rule sets

--- rules_admin/rules_admin.js
+++ rules_admin/rules_admin.js
@@ -13,3 +13,37 @@ Drupal.behaviors.RulesAdminSetAddArg = function (context) {
     });
   });
 };
+
+Drupal.behaviors.RulesAdminMachineName = function (context) {
+  // Add rule form machine-readable JS
+  $('#edit-label').addClass('processed').after(' <small class="rules-name-suffix">&nbsp;</small>');
+  $('#edit-name').parents('.form-item').hide();
+
+  // Add a click function to our rules name suffix.
+  $('.rules-name-suffix').click(function() {
+        $('#edit-name').parents('.form-item').show();
+        $('.rules-name-suffix').hide();
+        $('#edit-label').unbind('keyup');
+        return false;
+  });
+
+  $('#edit-label').keyup(function() {
+    var machine = $(this).val().toLowerCase().replace(/[^a-z0-9]/g, '_').replace(/_+/g, '_').replace(/^[^a-z]/, 'a');
+    if (machine !== '') {
+      $('#edit-name').val(machine);
+      $('.rules-name-suffix').html(' ' + Drupal.t('Machine name:') + ' ' + machine + ' [').append($('<a href="#">'+ Drupal.t('Edit') +'</a>')).append(']');
+    }
+    else {
+      $('#edit-name').val(machine);
+      $('.rules-name-suffix').text('');
+    }
+  });
+  // If we already have a machine name filled in, then just use that.
+  if($('#edit-name').val() !== '') {
+    $('.rules-name-suffix').html(' ' + Drupal.t('Machine name:') + ' ' + $('#edit-name').val() + ' [').append($('<a href="#">'+ Drupal.t('Edit') +'</a>')).append(']');
+  }
+  // If there's nothing in the field, then we can just trigger the event.
+  else {
+    $('#edit-label').keyup();
+  }
+}
