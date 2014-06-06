2a2527c349c8362832fd834f62cc1c25bbf9afab
Nathanael Dewhurst <ndewhurst@digitalbungalow.com>
2014-06-06 14:32:38 -0500

    Issue #2281111: Porting 'Entity is of bundle (entity_is_of_bundle)' condition to 8.x

---
+++ src/Plugin/Condition/EntityIsOfBundle.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\rules\Plugin\Condition\EntityIsOfBundle.
+ */
+
+namespace Drupal\rules\Plugin\Condition;
+
+use Drupal\Core\TypedData\TypedDataManager;
+use Drupal\rules\Context\ContextDefinition;
+use Drupal\rules\Engine\RulesConditionBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides an 'Entity is of bundle' condition.
+ *
+ * @Condition(
+ *   id = "rules_entity_is_of_bundle",
+ *   label = @Translation("Entity is of bundle")
+ * )
+ *
+ * @todo: Add access callback information from Drupal 7?
+ * @todo: Add group information from Drupal 7?
+ */
+class EntityIsOfBundle extends RulesConditionBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('typed_data_manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function contextDefinitions(TypedDataManager $typed_data_manager) {
+    $contexts['entity'] = ContextDefinition::create($typed_data_manager, 'entity')
+      ->setLabel(t('Entity'))
+      ->setDescription(t('Specifies the entity for which to evaluate the condition.'));
+
+    // @todo: Specify input type/options once config/UI questions are settled.
+    // @todo: Restrict data input method to "direct user input" rather than allowing data selector or similar method.
+    $contexts['type'] = ContextDefinition::create($typed_data_manager, 'string')
+      ->setLabel(t('Type'))
+      ->setDescription(t('The type of the evaluated entity.'));
+
+    // @todo: Specify input type/options once config/UI questions are settled.
+    // @todo: Restrict data input method to "direct user input" rather than allowing data selector or similar method.
+    $contexts['bundle'] = ContextDefinition::create($typed_data_manager, 'string')
+      ->setLabel(t('Bundle'))
+      ->setDescription(t('The bundle of the evaluated entity.'));
+
+    return $contexts;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function summary() {
+    return $this->t('Entity is of bundle');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function evaluate() {
+    $provided_entity = $this->getContextValue('entity');
+    $specified_type = $this->getContextValue('type');
+    $specified_bundle = $this->getContextValue('bundle');
+    $entity_type = $provided_entity->getEntityTypeId();
+    $entity_bundle = $provided_entity->bundle();
+    // Check to see whether the entity's bundle and type match the specified
+    //  values.
+    return $entity_bundle == $specified_bundle && $entity_type == $specified_type;
+  }
+}
