6865c9b96b6a4114f43a2e662ca17dd5dea0edcf
Calin Marian <calin.marian@dreamproduction.com>
2015-08-09 13:42:08 +0200

    Updated the documentation of the ExecutablePluginTrait

--- src/Core/ExecutablePluginTrait.php
+++ src/Core/ExecutablePluginTrait.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Contains \Drupal\rules\Core\RulesPluginTrait.
+ * Contains \Drupal\rules\Core\ExecutablePluginTrait.
  */
 
 namespace Drupal\rules\Core;
@@ -10,28 +10,38 @@ namespace Drupal\rules\Core;
 use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
 
 /**
- * Offers common methods for context plugin implementers.
+ * Offers common methods for executable plugins.
  */
 trait ExecutablePluginTrait {
 
   /**
-   * {@inheritdoc}
+   * Get the translated label from the plugin definition.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   *   Thrown if the label is not defined for the plugin.
+   *
+   * @return string
+   *   The label of the plugin.
    */
-  private function getLabelValue() {
+  protected function getLabelValue() {
     $definition = $this->getPluginDefinition();
     if (empty($definition['label'])) {
-      throw new InvalidPluginDefinitionException('The label is not defined.');
+      throw new InvalidPluginDefinitionException('The label is not defined for plugin ' . $this->getPluginId() . ' (' . __CLASS__ . ').');
     }
-    return $definition['label'];
+    // The label can be an instance of
+    // \Drupal\Core\StringTranslation\TranslationWrapper here, so make sure to
+    // always return a primitive string representation here.
+    return (string) $definition['label'];
   }
 
   /**
    * Get the translated summary from the label annotation.
    *
-   * @throws \Drupal\Component\Plugin\Exception\ContextException
-   *  Thrown if a summary was not set.
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   *   Thrown if a summary was not set.
    *
    * @return string
+   *   The summary of the plugin.
    */
   public function summary() {
     return $this->getLabelValue();
