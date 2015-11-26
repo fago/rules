509b9b10d8200694324981cdb16f7dfc93cc53ce
Erin McLaughlin <emclaughlin@genuineinteractive.com>
2015-11-26 09:30:10 +0100

    Issue #2572119: doEvaluate() conversion for EntityHasField

--- src/Plugin/Condition/EntityHasField.php
+++ src/Plugin/Condition/EntityHasField.php
@@ -7,6 +7,7 @@
 
 namespace Drupal\rules\Plugin\Condition;
 
+use Drupal\Core\Entity\FieldableEntityInterface;
 use Drupal\rules\Core\RulesConditionBase;
 
 /**
@@ -33,11 +34,17 @@ use Drupal\rules\Core\RulesConditionBase;
 class EntityHasField extends RulesConditionBase {
 
   /**
-   * {@inheritdoc}
+   * Checks if a given entity has a given field.
+   *
+   * @param \Drupal\Core\Entity\FieldableEntityInterface $entity
+   *   The entity to check for the provided field.
+   * @param string $field
+   *   The field to check for on the entity.
+   *
+   * @return bool
+   *   TRUE if the provided entity has the provided field.
    */
-  public function evaluate() {
-    $entity = $this->getContextValue('entity');
-    $field = $this->getContextValue('field');
+  protected function doEvaluate(FieldableEntityInterface $entity, $field) {
     return $entity->hasField($field);
   }
 
