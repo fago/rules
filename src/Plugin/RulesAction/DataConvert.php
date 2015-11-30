5f4fc9e5d82c1bc4af4651714fc5c11a382c6cc7
Andreas Tasch <andy.tasch@gmail.com>
2015-11-30 17:05:05 +0100

    Issue #2572119: Convert execute to doExecute() for DataConvert action

--- src/Plugin/RulesAction/DataConvert.php
+++ src/Plugin/RulesAction/DataConvert.php
@@ -43,13 +43,15 @@ class DataConvert extends RulesActionBase {
 
   /**
    * Executes the plugin.
+   *
+   * @param mixed $value
+   *   The input value.
+   * @param string $target_type
+   *   The target type the value should be converted into.
+   * @param string $rounding_behavior
+   *   The behaviour for rounding.
    */
-  public function execute() {
-    $value = $this->getContextValue('value');
-
-    $target_type = $this->getContextValue('target_type');
-    $rounding_behavior = $this->getContextValue('rounding_behavior');
-
+  protected function doExecute($value, $target_type, $rounding_behavior) {
     // @todo: Add support for objects implementing __toString().
     if (!is_scalar($value)) {
       throw new \InvalidArgumentException('Only scalar values are supported.');
