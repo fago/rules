3a9df9ba50747ea7e2d557439c8af5d043843dcc
ndf <niels.defeyter@bluecarpet.eu>
2015-12-04 10:31:07 +0100

    Issue #2624848: Port event "System watchdog log entry is created" to D8

---
+++ src/Event/SystemLoggerEvent.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\rules\Event\SystemLoggerEvent.
+ */
+
+namespace Drupal\rules\Event;
+
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+/**
+ * Event that is fired when a logger-item is created.
+ */
+class SystemLoggerEvent extends GenericEvent {
+
+  const EVENT_NAME = 'rules_system_logger_event';
+
+}
---
+++ src/Logger/RulesLog.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\rules\Logger\RulesLog.
+ */
+
+namespace Drupal\rules\Logger;
+
+use Drupal\Core\Logger\LogMessageParserInterface;
+use Drupal\Core\Logger\RfcLoggerTrait;
+use Drupal\rules\Event\SystemLoggerEvent;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * Dispatches new logger-items as SystemLoggerEvent.
+ */
+class RulesLog implements LoggerInterface {
+  use RfcLoggerTrait;
+
+  /**
+   * The dispatcher.
+   *
+   * @var \Symfony\Component\EventDispatcher\EventDispatcherInterface
+   */
+  protected $dispatcher;
+
+  /**
+   * The message's placeholders parser.
+   *
+   * @var \Drupal\Core\Logger\LogMessageParserInterface
+   */
+  protected $parser;
+
+  /**
+   * Constructs a new instance.
+   *
+   * @param EventDispatcherInterface $dispatcher
+   *   An EventDispatcherInterface instance.
+   * @param \Drupal\Core\Logger\LogMessageParserInterface $parser
+   *   The parser to use when extracting message variables.
+   */
+  public function __construct(EventDispatcherInterface $dispatcher, LogMessageParserInterface $parser) {
+    $this->dispatcher = $dispatcher;
+    $this->parser = $parser;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @todo: create a TypedData logger-entry object: https://www.drupal.org/node/2625238
+   */
+  public function log($level, $message, array $context = array()) {
+    // Remove any backtraces since they may contain an unserializable variable.
+    unset($context['backtrace']);
+
+    // Convert PSR3-style messages to SafeMarkup::format() style, so they can be
+    // translated too in runtime.
+    $message_placeholders = $this->parser->parseMessagePlaceholders($message, $context);
+
+    $logger_entry = array(
+      'uid' => $context['uid'],
+      'type' => $context['channel'],
+      'message' => $message,
+      'variables' => $message_placeholders,
+      'severity' => $level,
+      'link' => $context['link'],
+      'location' => $context['request_uri'],
+      'referer' => $context['referer'],
+      'hostname' => $context['ip'],
+      'timestamp' => $context['timestamp'],
+    );
+
+    // Dispatch logger_entry event.
+    $event = new SystemLoggerEvent($logger_entry, ['logger_entry' => $logger_entry]);
+    $this->dispatcher->dispatch(SystemLoggerEvent::EVENT_NAME, $event);
+  }
+
+}
