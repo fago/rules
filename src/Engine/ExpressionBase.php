18ca6b02ed855a82c98ea18b7e694f0655aab5e5
Klaus Purer <klaus.purer@gmail.com>
2016-02-01 15:13:41 +0100

    Removed unused methods from ExpressionBase

--- src/Engine/ExpressionBase.php
+++ src/Engine/ExpressionBase.php
@@ -8,10 +8,9 @@
 namespace Drupal\rules\Engine;
 
 use Drupal\Core\Plugin\PluginBase;
-use Drupal\rules\Context\ContextDefinition;
 
 /**
- * Base class for rules actions.
+ * Base class for rules expressions.
  */
 abstract class ExpressionBase extends PluginBase implements ExpressionInterface {
 
@@ -37,47 +36,6 @@ abstract class ExpressionBase extends PluginBase implements ExpressionInterface 
   protected $configEntityId;
 
   /**
-   * Overrides the parent constructor to populate context definitions.
-   *
-   * Expression plugins can be configured to have arbitrary context definitions.
-   *
-   * @param array $configuration
-   *   The plugin configuration, i.e. an array with configuration values keyed
-   *   by configuration option name. The special key 'context_definitions' may
-   *   be used to initialize the context definitions by setting it to an array
-   *   of definitions keyed by context names.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
-    if (isset($configuration['context_definitions'])) {
-      $plugin_definition['context'] = $this->createContextDefinitions($configuration['context_definitions']);
-    }
-    if (isset($configuration['provided_definitions'])) {
-      $plugin_definition['provides'] = $this->createContextDefinitions($configuration['provided_definitions']);
-    }
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-  }
-
-  /**
-   * Converts a context definition configuration array into objects.
-   *
-   * @param array $configuration
-   *   The configuration properties for populating the context definition
-   *   object.
-   *
-   * @return \Drupal\Core\Plugin\Context\ContextDefinitionInterface[]
-   *   A list of context definitions with the same keys.
-   */
-  protected function createContextDefinitions(array $configuration) {
-    return array_map(function ($definition_array) {
-      return ContextDefinition::createFromArray($definition_array);
-    }, $configuration);
-  }
-
-  /**
    * Executes a rules expression.
    */
   public function execute() {
@@ -87,13 +45,6 @@ abstract class ExpressionBase extends PluginBase implements ExpressionInterface 
     // Save specifically registered variables in the end after execution.
     $state->autoSave();
     return $result;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function refineContextDefinitions() {
-    // Do not refine anything by default.
   }
 
   /**
