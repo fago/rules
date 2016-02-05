46f1fbe2687db9873ccbe9f046d31d4ff29fcb46
milkovsky <a.milkovsky@gmail.com>
2016-02-05 18:31:42 +0100

    Improved EventHandlerTest WIP.

--- tests/src/Kernel/EventHandlerTest.php
+++ tests/src/Kernel/EventHandlerTest.php
@@ -19,7 +19,10 @@ use Drupal\rules\Context\ContextConfig;
  */
 class EventHandlerTest extends RulesDrupalTestBase {
 
-  public static $modules = ['rules', 'rules_test', 'system', 'node', 'field'];
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['rules', 'rules_test', 'system', 'node', 'field', 'user'];
 
   /**
    * The entity storage for Rules config entities.
@@ -29,24 +32,30 @@ class EventHandlerTest extends RulesDrupalTestBase {
   protected $storage;
 
   /**
+   * A node used for testing.
+   *
+   * @var \Drupal\node\NodeInterface
+   */
+  protected $node;
+
+  /**
    * {@inheritdoc}
    */
   public function setUp() {
     parent::setUp();
 
-    $this->installConfig(['system']);
-    $this->installConfig(['field']);
     $this->installSchema('system', ['sequences']);
+    $this->installEntitySchema('user');
     $this->installEntitySchema('node');
+    $this->installConfig(['field']);
 
     $this->storage = $this->container->get('entity_type.manager')->getStorage('rules_reaction_rule');
-  }
 
-  /**
-   * Tests EventHandlerEntityBundle configuration.
-   */
-  public function testEntityBundleHandlerConfiguration() {
-    // Create a multi-value integer field for testing.
+    $entity_type_manager = $this->container->get('entity_type.manager');
+    $entity_type_manager->getStorage('node_type')
+      ->create(['type' => 'page'])
+      ->save();
+
     FieldStorageConfig::create([
       'field_name' => 'field_integer',
       'type' => 'integer',
@@ -59,12 +68,17 @@ class EventHandlerTest extends RulesDrupalTestBase {
       'bundle' => 'page',
     ])->save();
 
-    // Create test node with a bundle and field.
-    $entity_type_manager = $this->container->get('entity_type.manager');
-    $entity_type_manager->getStorage('node_type')
-      ->create(['type' => 'page'])
-      ->save();
+    $this->node = $entity_type_manager->getStorage('node')
+      ->create([
+        'title' => 'test',
+        'type' => 'page',
+      ]);
+  }
 
+  /**
+   * Tests EventHandlerEntityBundle configuration.
+   */
+  public function testEntityBundleHandlerConfiguration() {
     // Create rule with an action 'rules_entity_presave:node–-page'.
     $rule = $this->expressionManager->createRule();
     $config_entity = $this->storage->create([
@@ -82,33 +96,9 @@ class EventHandlerTest extends RulesDrupalTestBase {
    * Tests EventHandlerEntityBundle execution.
    */
   public function testEntityBundleHandlerExecution() {
-    // Create a multi-value integer field for testing.
-    FieldStorageConfig::create([
-      'field_name' => 'field_integer',
-      'type' => 'integer',
-      'entity_type' => 'node',
-      'cardinality' => FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED,
-    ])->save();
-    FieldConfig::create([
-      'field_name' => 'field_integer',
-      'entity_type' => 'node',
-      'bundle' => 'page',
-    ])->save();
+    $this->node->field_integer->setValue(['0' => 1, '1' => 2]);
 
-    // Create test node with a bundle and field.
-    $entity_type_manager = $this->container->get('entity_type.manager');
-    $entity_type_manager->getStorage('node_type')
-      ->create(['type' => 'page'])
-      ->save();
-    $node = $entity_type_manager->getStorage('node')
-      ->create([
-        'title' => 'test',
-        'type' => 'page',
-        // @todo set node.field_integer.0.value.
-      ])
-      ->save();
-
-    // Create rule with an action 'rules_entity_presave:node–-page'.
+    // Create rule with an action 'rules_entity_presave:node--page'.
     $rule = $this->expressionManager->createRule();
     $rule->addAction('rules_test_log',
       ContextConfig::create()
@@ -117,17 +107,16 @@ class EventHandlerTest extends RulesDrupalTestBase {
     $config_entity = $this->storage->create([
       'id' => 'test_rule',
       'expression_id' => 'rules_rule',
-      'event' => 'rules_entity_presave:node-–page',
+      'event' => 'rules_entity_presave:node--page',
       'configuration' => $rule->getConfiguration(),
     ]);
     $config_entity->save();
 
-    // @todo Dispatch presave.
-    $dispatcher = $this->container->get('event_dispatcher');
-    // $dispatcher->dispatch('entity_presave', $node);
+    // @todo Better dispatch presave event.
+    $this->node->save();
 
-    // @todo Test that the action in the rule logged node value.
-    // $this->assertRulesLogEntryExists('test_user');
+    // Test that the action in the rule logged node value.
+    $this->assertRulesLogEntryExists('1');
   }
 
 }
