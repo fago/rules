0e7048d637a844472794143aacd58d7ea922bea1
Klaus Purer <klaus.purer@gmail.com>
2016-03-27 14:32:13 +0200

    Issue #2659954: Switch Travis CI sudo container mode to avoid random functional test fails

--- tests/src/Functional/RulesBrowserTestBase.php
+++ tests/src/Functional/RulesBrowserTestBase.php
@@ -38,6 +38,11 @@ abstract class RulesBrowserTestBase extends BrowserTestBase {
         $this->fail(sprintf('%s: %s in %s on line %d', $error_info[1], (string) $error_info[0], $error_info[2]['file'], $error_info[2]['line']));
       }
     }
+    // Temporary core hack to get debug information when random test fails
+    // occur. See https://www.drupal.org/node/2659954
+    if ($this->getSession()->getStatusCode() == 555) {
+      $this->fail($this->getSession()->getPage()->getContent());
+    }
   }
 
   /**
