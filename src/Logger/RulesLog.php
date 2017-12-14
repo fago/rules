1cf6573f4f8ce17b567817c60ad2b29b267a9b88
amateescu <amateescu@729614.no-reply.drupal.org>
2017-12-14 17:22:24 +0100

    Issue #2803823 by amateescu: Rules' logger needs to take care of its internal properties on serialization

--- src/Logger/RulesLog.php
+++ src/Logger/RulesLog.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\rules\Logger;
 
+use Drupal\Core\DependencyInjection\DependencySerializationTrait;
 use Drupal\Core\Logger\LogMessageParserInterface;
 use Drupal\Core\Logger\RfcLoggerTrait;
 use Drupal\rules\Event\SystemLoggerEvent;
@@ -12,7 +13,9 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
  * Dispatches new logger-items as SystemLoggerEvent.
  */
 class RulesLog implements LoggerInterface {
+
   use RfcLoggerTrait;
+  use DependencySerializationTrait;
 
   /**
    * The dispatcher.
